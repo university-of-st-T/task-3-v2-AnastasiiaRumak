1e57d589ea595d7bfe596d6e46c9e15294f46ef2
AnastasiiaRumak <56265145+AnastasiiaRumak@users.noreply.github.com>
2026-03-06 14:57:13 +0300

    Update calculate_total.py

--- calculate_total.py
+++ calculate_total.py
@@ -2,12 +2,12 @@ def calculate_total(prices, *discounts, **options):
     tax = options.get('tax', 0)
     round_to = options.get('round_to', 2)
 
-    total = sum(prices)
+    total = float(sum(prices))
 
     for discount in discounts:
-        total *= (1 - discount / 100)
+        total *= (1 - discount / 100.0)
 
-    total *= (1 + tax / 100)
+    total *= (1 + tax / 100.0)
 
     if round_to is not None:
         total = round(total, round_to)
